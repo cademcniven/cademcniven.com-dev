4fbb1484a4b9cd48a5977f87d5f661d147b4ceed
cademcniven <cademcniven@gmail.com>
2021-05-09 10:28:11 -0700

    Added html/css minifiers, and inlined css

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 const { DateTime } = require("luxon");
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+const CleanCSS = require("clean-css");
+const htmlmin = require("html-minifier");
 
 module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/japanese_glosser");
@@ -16,8 +18,29 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addWatchTarget("src/**/*.png");
     eleventyConfig.addWatchTarget("src/**/*.gif");
 
+    //Get human readable dates for index.html
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_FULL);
+    });
+
+    //Minify css
+    eleventyConfig.addFilter("cssmin", function (code) {
+        return new CleanCSS({}).minify(code).styles;
+    });
+
+    //Minify html
+    eleventyConfig.addTransform("htmlmin", function (content, outputPath) {
+        // Eleventy 1.0+: use this.inputPath and this.outputPath instead
+        if (outputPath && outputPath.endsWith(".html")) {
+            let minified = htmlmin.minify(content, {
+                useShortDoctype: true,
+                removeComments: true,
+                collapseWhitespace: true
+            });
+            return minified;
+        }
+
+        return content;
     });
 
     eleventyConfig.addPlugin(syntaxHighlight);
